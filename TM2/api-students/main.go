e91e99f6b70aa39a44a9e9b8e10f3cb5438bc37c
Nabil Hakim Alfikri <nabilalfikri8@gmail.com>
2026-08-24 00:25:41 +0700

    TM2: middleware & routing

--- TM2/api-students/main.go
+++ TM2/api-students/main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
+
+var metodeBerbody = map[string]bool{
+	fiber.MethodPost:  true,
+	fiber.MethodPut:   true,
+	fiber.MethodPatch: true,
+}
+
+func requireJSON(c *fiber.Ctx) error {
+	if metodeBerbody[c.Method()] {
+		ct := c.Get("Content-Type")
+		if !strings.HasPrefix(ct, fiber.MIMEApplicationJSON) {
+			return fail(c, fiber.StatusUnsupportedMediaType,
+				"Content-Type harus application/json")
+		}
+	}
+	return c.Next()
+}
 
 func main() {
 	app := fiber.New(fiber.Config{
@@ -33,6 +52,20 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!")
 	})
+
+	api := app.Group("api/v1")
+
+	api.Get("/health", func(c *fiber.Ctx) error {
+		return ok(c, "Server berjalan", fiber.Map{"timestamp": time.Now()})
+	})
+
+	s := api.Group("/students", requireJSON)
+	s.Get("/", listStudents)
+	s.Get("/:id", getStudent)
+	s.Post("/", createStudent)
+	s.Put("/:id", replaceStudent)
+	s.Patch("/:id", patchStudent)
+	s.Delete("/:id", deleteStudent)
 
 	app.Use(func(c *fiber.Ctx) error {
 		return fail(c, fiber.StatusNotFound, "Endpoint tidak ditemukan")
